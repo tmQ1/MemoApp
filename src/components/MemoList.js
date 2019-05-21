49d3a26a5471ac62bbfa358ff1a0306b7f105ed4
tmQ1 <211k523k@gmail.com>
2019-05-22 07:55:37 +0900

    Refactoring MemoList

---
+++ src/components/MemoList.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class MemoList extends React.Component {
+    render() {
+        return (
+            <View>
+                <View style={styles.memoList}>
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講義のアイテム</Text>
+                        <Text style={styles.memoDate}>2017/10/10</Text>
+                    </View>
+                </View>
+
+                <View style={styles.memoList}>
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講義のアイテム</Text>
+                        <Text style={styles.memoDate}>2017/10/10</Text>
+                    </View>
+                </View>
+
+                <View style={styles.memoList}>
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講義のアイテム</Text>
+                        <Text style={styles.memoDate}>2017/10/10</Text>
+                    </View>
+                </View>
+
+                <View style={styles.memoList}>
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講義のアイテム</Text>
+                        <Text style={styles.memoDate}>2017/10/10</Text>
+                    </View>
+                </View>
+
+                <View style={styles.memoList}>
+                    <View style={styles.memoListItem}>
+                        <Text style={styles.memoTitle}>講義のアイテム</Text>
+                        <Text style={styles.memoDate}>2017/10/10</Text>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    memoList: {
+        width: '100%',
+        flex: 1,
+    },
+
+    memoListItem: {
+        padding: 16,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+        backgroundColor: '#FFF',
+    },
+
+    memoTitle: {
+        fontSize: 18,
+        marginBottom: 4,
+    },
+
+    memoDate: {
+        fontSize: 12,
+        color: '#a2a2a2',
+    },
+
+});
+
+
+
+
+export default MemoList;
